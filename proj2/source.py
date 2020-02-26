dbd0aeebf8405ccf07c28fd81b07580bac48980e
Jeff Byrnes <Jrbyrnes1989@gmail.com>
2020-02-26 15:20:07 -0800

    LTSM model

--- proj2/source.py
+++ proj2/source.py
@@ -16,10 +16,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, roc_curve, auc
 from sklearn.linear_model import LogisticRegression
 import matplotlib.pyplot as plt
+import scikitplot as skplt
 
 from tensorflow.keras import optimizers
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.layers import Dense, Activation, Embedding, LSTM
 from tensorflow.keras.callbacks import EarlyStopping
 from tensorflow.keras.callbacks import ModelCheckpoint
 
@@ -46,7 +47,7 @@ def to_sequences(seq_size, data):
         #print(i)
         window = data[i:(i+SEQUENCE_SIZE)]
         after_window = data[i+SEQUENCE_SIZE]
-        window = [[x] for x in window]
+        window = [x for x in window]
         #print("{} - {}".format(window,after_window))
         x.append(window)
         y.append(after_window)
@@ -55,8 +56,31 @@ def to_sequences(seq_size, data):
 
 SEQUENCE_SIZE = 7
 x_train,y_train = to_sequences(SEQUENCE_SIZE,params_train)
-obs_train = [item[4] for item in y_train]
-
+obs_train = np.asarray([item[4] for item in y_train])
 
 x_test,y_test = to_sequences(SEQUENCE_SIZE,params_test)
-obs_test = [item[4] for item in y_test]
+obs_test = np.asarray([item[4] for item in y_test])
+
+print("Shape of x_train: {}".format(x_train.shape))
+print("Shape of x_test: {}".format(x_test.shape))
+print("Shape of y_train: {}".format(obs_train.shape))
+print("Shape of y_test: {}".format(obs_test.shape))
+
+
+model = Sequential()
+
+model.add(LSTM(64, dropout=0.1, recurrent_dropout=0.1, input_shape=(SEQUENCE_SIZE, 5)))
+model.add(Dense(32))
+model.add(Dense(1))
+
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
+print('Train...')
+
+model.fit(x_train,obs_train,validation_data=(x_test,obs_test), callbacks=[monitor],verbose=2, epochs=3)  
+
+pred = model.predict(x_test)
+score = np.sqrt(metrics.mean_squared_error(pred,obs_test))
+print("Score (RMSE): {}".format(score))
+
